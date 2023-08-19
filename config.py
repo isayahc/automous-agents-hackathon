955c631c002976223dc4bdb1f6ccd081841565c6
isayhc <isayahculbertson@gmail.com>
2023-08-19 15:42:58 -0400

    adding TELEGRAM_BOT_KEY to config.py

--- config.py
+++ config.py
@@ -7,10 +7,12 @@ dotenv.load_dotenv()
 
 ELEVENLABS_API_KEY = os.environ["ELEVENLABS_API_KEY"]
 OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
+TELEGRAM_BOT_KEY = os.environ['TELEGRAM_BOT_KEY']
 
 
 elevenlabs.set_api_key(ELEVENLABS_API_KEY)
 openai.api_key = OPENAI_API_KEY
+
 
 
 
